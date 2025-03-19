5699f3ffb5f670dd199264bf8321414b3cf16e79
Matías <matias.abarca2018@gmail.com>
2025-03-18 22:27:26 -0300

    Se implementa react router

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
-import { NavBar } from '../components/layout/NavBar'
 
 export const Home = () => {
     return (
-        <>
-            <NavBar />
-            <main className='container-lg' >
-                <h1>Página Home</h1>
-            </main>
-        </>
+        <main className='container-lg' >
+            <h1>Página Home</h1>
+        </main>
 
     )
 }
