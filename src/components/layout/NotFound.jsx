9ebd0b888ef903b1ae86280b6c01d08feb21e54f
Skinng <felipefaundez.vasquez@gmail.com>
2025-03-18 23:20:28 -0300

    Se realizan modificaciones en el componente NotFound, se agrega color background y se centra el mensaje

--- src/components/layout/NotFound.jsx
+++ src/components/layout/NotFound.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
-
+import React from "react";
+import { Link } from "react-router-dom";
 export const NotFound = () => {
   return (
-    <h2>Página no encontrada! :C</h2>
-  )
-}
+    <>
+      <div className="d-flex justify-content-center align-items-center bg-warning text-dark text-center min-vh-100">
+        <h2>Página no encontrada! 😢</h2>
+        <Link to={"/"} > <p><a className="link-offset-2 link-underline link-underline-opacity-0">Volver a la página principal</a></p> </Link>
+      </div>
+    </>
+  );
+};
