5dbb41605ffafb59560a427b552c1f18671049a9
Matías <matias.abarca2018@gmail.com>
2025-03-24 11:37:24 -0300

    Se agrega el fetch de api users a tabla de todos los usuarios

--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { fetchApi } from '../../services/fetchApi';
+
+
+const urlApiAllUsers = 'http://localhost:8080/api/v1/users';
 
 export const Users = () => {
     
+    const [users, setusers] = useState([]);
+
+
+    useEffect(() => {
+      fetchApi(urlApiAllUsers)
+        .then( data => {
+            setusers( data );
+        })
+    
+    }, [])
+    
+
     return (
         <>
             <h2>Listado de usuarios</h2>
@@ -24,10 +40,52 @@ export const Users = () => {
                         <th scope='col' >
                             Roles
                         </th>
+                        <th scope='col' >
+                            Acciones
+                        </th>
                     </tr>
                 </thead>
                 <tbody>
+                    {
 
+                        users && users.map( user => {
+                            return (
+                                <tr key={ user.id } >
+                                    <td>
+                                        { user.id }
+                                    </td>
+                                    <td>
+                                        { user.username }
+                                    </td>
+                                    <td>
+                                        { user.email }
+                                    </td>
+                                    <td>
+                                        { user.phoneNumber }
+                                    </td>
+                                    <td>
+                                        {
+                                            user.roles.map( role => (
+                                                <span key={ role.id } className='badge bg-primary me-2' >
+                                                    { role.name }
+                                                </span>
+                                            ))
+                                        }
+                                    </td>
+                                    <td>
+                                        <button className='btn btn-primary' >
+                                            Editar
+                                        </button>
+                                        <button className='btn btn-danger ms-2' >
+                                            Borrar
+                                        </button>
+                                    </td>
+
+                                </tr>
+
+                            )
+                        } )
+                    }
                 </tbody>
             </table>
         </>
